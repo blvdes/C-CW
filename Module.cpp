f4e6aab60db3da321a8ff39fcb3639e856d5a8b1
blvdes <112328919+blvdes@users.noreply.github.com>
2025-11-30 14:04:24 +0000

    Add files via upload

---
+++ Module.cpp
@@ -0,0 +1,5 @@
+#include "Module.h"
+
+// Constructor
+Module::Module(const std::string& code, const std::string& name, int credits) : _code(code), _name(name), _credits(credits) {}
+
---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+// Imports for main
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+
+using namespace std;
+
+#endif
