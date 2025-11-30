974c951ebdd8ad0bb5f6042a6b9d37db9de24fb8
blvdes <112328919+blvdes@users.noreply.github.com>
2025-11-30 13:31:51 +0000

    Add files via upload

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+// Imports for main
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+
+#endif
